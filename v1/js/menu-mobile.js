dcdfbf677a1ef72f54c7116c604ef9c1e30b18f1
KalelBaiao <kalelbaiao2@gmail.com>
2022-08-16 01:21:59 -0400

    atualização

--- v1/js/menu-mobile.js
+++ v1/js/menu-mobile.js
@@ -1,5 +1,5 @@
 const btnMobile = document.querySelector('.btn-mobile')
-console.log(btnMobile)
+// console.log(btnMobile)
 btnMobile.addEventListener('click', () => {
     const nav = document.querySelector('.Dnav')
     nav.classList.toggle('active')
